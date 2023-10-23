c17bcf01fc6b003da9713d7e15ded922e1b57f3b
ogCunci <bolykydani@gmail.com>
2023-10-23 11:43:26 +0200

    E-mail sending now works properly

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -4,22 +4,22 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req) {
-  const data= req.json();
-  const { email, subject, message } = data;
+  const data = await req.json();
+  console.log(data)
 
   try {
-    const data = await resend.emails.send({
+    const response = await resend.emails.send({
       from: fromEmail,
-      to: ["bolykydani@gmail.com", email],
-      subject: subject,
-      html: (
+      to: "bolykydani@gmail.com",
+      subject: data.subject,
+      react: (
         <>
-          <h1>{subject}</h1>
-          <p>{message}</p>
+          <h3>{`Message from: ${data.email}`}</h3>
+          <p>{data.message}</p>
         </>
       ),
     });
-
+    console.log(response);
     return Response.json({ message: "Message sent!" });
   } catch (error) {
     console.error(error);
